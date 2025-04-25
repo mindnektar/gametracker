5e6b79d3bfdef6b5ce402cbe960297ae772f41c4
Martin Denk <martin.denk@carloandfriends.de>
2025-04-25 16:06:01 +0200

    improve ai prompt

--- server/services/ai.js
+++ server/services/ai.js
@@ -6,7 +6,7 @@ export default async (title, system) => {
         const ai = new GoogleGenAI({ apiKey: config.ai.apiKey });
         const response = await ai.models.generateContent({
             model: 'gemini-2.0-flash',
-            contents: `Please provide a description of about 100 words, release year, developer, and genres for the ${system} game ${title}, in JSON format.`,
+            contents: `Please provide a spoiler-free description of about 100 words, release year and developer for the ${system} game ${title}, in JSON format.`,
         });
 
         return JSON.parse(response.text.replace(/```(json)?/g, '').trim());
